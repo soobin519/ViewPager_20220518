ce180e61a3a44e8d8d268a35a4449c8c776bf114
soobin519 <soobin0519@naver.com>
2022-05-18 13:25:02 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/asianaidt/viewpager_20220518/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.asianaidt.viewpager_20220518
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
